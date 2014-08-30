f60c30524f6eb87ee7f93565102ad4579be388e7
Randa Zraik <r.b.z1992@hotmail.com>
2014-08-30 23:16:36 +0300

    added source code

---
+++ StringCalculatorKata.Tests/StringCalculatorTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StringCalculatorKata.Tests
+{
+    public class StringCalculatorTests
+    {
+        [Fact]
+        public void AddReturnsZeroOnEmptyString()
+        {
+            Assert.Equal(0, StringCalculator.Add(""));
+        }
+    }
+}
